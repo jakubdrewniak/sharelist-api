2d277110979c06ed8999bdfc24c5c21077d8a02c
jakubdrewniak <drewniak.jakub@gmail.com>
2022-03-17 13:03:51 +0100

    Websocket connection with client POC

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
-import path from "path";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-// TO DO: remove, POC only
-const publicDirectoryPath = path.join(__dirname, "../../public");
-app.use(express.static(publicDirectoryPath));
+const io = new Server(server,
+  {
+    cors: {
+      origin: "http://localhost:4200",
+      methods: ["GET", "POST"]
+    }
+  }
+  );
 
 io.on("connection", (socket) => {
-  console.log("New WebSocket connection");
+  console.count("New WebSocket connection");
 
   socket.on("join", (options, callback) => {
     console.log("client joined", options);
+
+    socket.emit('message', {msg: 'Hello! This is first messahe from server!'})
 
     callback();
   });
