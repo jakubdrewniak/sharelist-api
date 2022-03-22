e9a27bb87c43de00b5c708e1292571316f2258d9
jakubdrewniak <drewniak.jakub@gmail.com>
2022-03-22 16:19:52 +0100

    Catalog router- POST

--- src/routers/catalogs.ts
+++ src/routers/catalogs.ts
@@ -4,8 +4,19 @@ import { Catalog } from "../models/catalog";
 const router = Router();
 
 router.get("/catalogs", async (req, res) => {
-  const catalogs = await Catalog.find()
-  return res.send(catalogs)
+  const catalogs = await Catalog.find();
+  return res.send(catalogs);
+});
+
+router.post("/catalogs", async (req, res) => {
+  const catalog = new Catalog({ ...req.body });
+
+  try {
+    await catalog.save();
+    res.status(201).send(catalog._id);
+  } catch (e) {
+    res.status(400).send(e)
+  }
 });
 
 export { router as catalogsRouter };
