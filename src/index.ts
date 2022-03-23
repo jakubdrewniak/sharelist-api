4edab3460084208c98608c13ec2de68ad9fecf28
jakubdrewniak <drewniak.jakub@gmail.com>
2022-03-23 12:37:05 +0100

    Websocket: create room, join client to room, add new elements to catalog

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
 import express from "express";
-import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
 import { catalogsRouter } from "./routers/catalogs";
 import { connectToDB } from "./db/mongoose";
+import { initWebsocket } from "./websocket";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -19,29 +19,10 @@ app.use(
 );
 app.use(catalogsRouter);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:4200",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.count("New WebSocket connection");
-
-  socket.on("join", (options, callback) => {
-    console.log("client joined", options);
-
-    socket.emit("message", {
-      msg: "Hello! This is first messahe from server!",
-    });
-
-    callback();
-  });
-});
+initWebsocket(server);
 
 connectToDB().then(() => {
-  server.listen(PORT, () => {
+  server.listen(PORT, async () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
   });
 });
---
+++ src/websocket.ts
@@ -0,0 +1,46 @@
+import { Server } from "socket.io";
+import { Catalog } from "./models/catalog";
+import http from "http";
+
+function initWebsocket(server: http.Server) {
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:4200",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("join", async (options, callback) => {
+      if (!options._id) {
+        callback("_id is required!");
+      }
+
+      await socket.join(options._id);
+
+      const catalog = await Catalog.findById(options._id);
+      io.to(options._id).emit("catalog", catalog);
+    });
+
+    socket.on("updateProducts", async (options, callback) => {
+      if (!options._id) {
+        return callback("_id is required!");
+      }
+      try {
+        const catalog = await Catalog.findById(options._id);
+        if (!catalog) {
+          return callback("Can't find catalog!");
+        }
+        catalog.products = [...options.products];
+        await catalog.save();
+
+        io.to(options._id).emit("catalog", catalog);
+        callback();
+      } catch (e) {
+        callback("Unexpected problem with products update");
+      }
+    });
+  });
+}
+
+export { initWebsocket };
