e592d96ac024d6276f1f60e0f7225cab7b3115b1
jakubdrewniak <drewniak.jakub@gmail.com>
2022-03-31 22:02:45 +0200

    "Completed" field in Product schema

--- src/models/product.ts
+++ src/models/product.ts
@@ -4,10 +4,12 @@ export interface IProduct {
   name: string;
   quantity?: number;
   unit?: string;
+  completed?: boolean 
 }
 
 export const productSchema = new Schema<IProduct>({
   name: { type: String, required: true, trim: true },
   quantity: { type: Number, required: false },
   unit: { type: String, required: false },
+  completed: {type: Boolean, required: false}
 });
